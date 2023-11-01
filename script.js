9a321287d2074b74393f29e23f8624f1b41b150a
Xiraeth <77112995+Xiraeth@users.noreply.github.com>
2023-11-02 00:39:06 +0200

    Create the rest of the html

--- script.js
+++ script.js
@@ -1,3 +1,18 @@
 "use strict";
 
 // const takeDmgBtn = document.querySelector(".");
+// localStorage.clear();
+
+const temp = document.querySelector(".tempHP span");
+console.log(temp.textContent);
+let tempHP = 0;
+
+temp.addEventListener("input", (e) => {
+  localStorage.setItem("tempHP", temp.textContent);
+});
+
+window.addEventListener("load", (e) => {
+  tempHP = localStorage.getItem("tempHP");
+  temp.textContent = localStorage.getItem("tempHP");
+  console.log(tempHP);
+});
